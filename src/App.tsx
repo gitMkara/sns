bb6e7795f98f9ee0ff15cbc05082449026d2f21f
murat kara <mail.muratkara@gmail.com>
2022-06-24 11:43:48 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import * as React from 'react';
 
 function App() {
+  const [message, setMessage] = React.useState<String>();
+  const [email, setEmail] = React.useState<String>();
+
+  const handlePublish = async (e: any) => {
+    console.log('message', message);
+    const url =
+      'https://924rac1219.execute-api.us-east-1.amazonaws.com/publish';
+    const data = {
+      message: message,
+    };
+    await axios
+      .post(url, data)
+      .then(function (response) {
+        console.log('publishing successful');
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log('publish error');
+        console.log(error);
+      });
+  };
+
+  const handleSubscribe = async (e: any) => {
+    const url =
+      'https://924rac1219.execute-api.us-east-1.amazonaws.com/subscribe';
+    const data = {
+      email: email,
+    };
+    await axios
+      .post(url, data)
+      .then(function (response) {
+        console.log('subcribing successful');
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log('subscribe error');
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input
+        type='text'
+        placeholder='email'
+        onChange={(e: any) => setEmail(e.target.value)}
+      />
+      <button onClick={handleSubscribe}>GET Subscribe</button>
+
+      <input
+        type='text'
+        placeholder='your message'
+        onChange={(e: any) => setMessage(e.target.value)}
+      />
+      <button onClick={handlePublish}>Publish Message</button>
     </div>
   );
 }
